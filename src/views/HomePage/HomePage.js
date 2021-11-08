752aa99325df4b299daa080481f5c084f10d1020
Mohara88s <mohara88s@gmail.com>
2021-11-08 12:29:45 +0200

    add home picture & plugs

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import { Link, useRouteMatch, useLocation } from 'react-router-dom';
 import styles from './HomePage.module.css';
+import anecdotTrainingsImg from '../../public/pictures/anecdot_trainings.jpg';
 
 export default function HomePage() {
   const { url } = useRouteMatch();
@@ -7,16 +8,66 @@ export default function HomePage() {
 
   return (
     <div className={styles.HomePage}>
-      <h2>Trainings:</h2>
-      <ul>
-        <li>
+      <h2>Choose trainings:</h2>
+      <ul className={styles.LinksList}>
+        <li className={styles.LinksList__item}>
           <Link
             to={{
               pathname: `${url}anecdotes-trainings/`,
               state: { from: location },
             }}
           >
-            <h3>Anecdotes trainings</h3>
+            <h3 className={styles.LinksList__name}>Anecdotes trainings</h3>
+            <img
+              src={anecdotTrainingsImg}
+              alt="Anecdotes trainings"
+              className={styles.LinksList__picture}
+            />
+          </Link>
+        </li>
+        <li className={styles.LinksList__item}>
+          <Link
+            to={{
+              pathname: `${url}anecdotes-trainings/`,
+              state: { from: location },
+            }}
+          >
+            <h3 className={styles.LinksList__name}>PLUG trainings</h3>
+            <img
+              src={anecdotTrainingsImg}
+              alt="Anecdotes trainings"
+              className={styles.LinksList__picture}
+            />
+          </Link>
+        </li>
+        <li className={styles.LinksList__item}>
+          <Link
+            to={{
+              pathname: `${url}anecdotes-trainings/`,
+              state: { from: location },
+            }}
+          >
+            <h3 className={styles.LinksList__name}>PLUG trainings</h3>
+            <img
+              src={anecdotTrainingsImg}
+              alt="Anecdotes trainings"
+              className={styles.LinksList__picture}
+            />
+          </Link>
+        </li>
+        <li className={styles.LinksList__item}>
+          <Link
+            to={{
+              pathname: `${url}anecdotes-trainings/`,
+              state: { from: location },
+            }}
+          >
+            <h3 className={styles.LinksList__name}>PLUG trainings</h3>
+            <img
+              src={anecdotTrainingsImg}
+              alt="Anecdotes trainings"
+              className={styles.LinksList__picture}
+            />
           </Link>
         </li>
       </ul>
